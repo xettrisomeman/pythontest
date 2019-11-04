f5a98f5e7ed59b19642c0686ee587011cc5253e1
webizfabulous <webizmoxx@gmail.com>
2019-11-04 14:28:54 +0545

    Added new testcase

--- tests/testfile1.py
+++ tests/testfile1.py
@@ -18,3 +18,23 @@ class MyTestPerson(unittest.TestCase):
         del self.person
 
 
+
+class MyAddTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.person = Add(3,2)
+
+    def test_add(self):
+        value = self.person.add()
+        self.assertGreater(value , 3) #it will pass because 3+2 is greater than 3
+
+    def test_substract(self):
+        value = self.person.substract()
+        self.assertFalse(value, 1) #it will fail because 3-2 is 1
+
+    def tearDown(self):
+        del self.person
+
+
+
+    
