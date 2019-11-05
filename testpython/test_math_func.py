63225c53c3eb60382585a4be46325cde8c42a532
webizfabulous <webizmoxx@gmail.com>
2019-11-05 11:04:12 +0545

    Added fixtures

--- testpython/test_math_func.py
+++ testpython/test_math_func.py
@@ -2,18 +2,25 @@ from math_func import StudentDB
 import pytest 
 
 
-def test_martin_data():
+
+@pytest.fixture
+def db():
+    print("=======SETUP=======")
     db = StudentDB()
-    db.connect('data.json')
+    db.connect("data.json")
+    return db
+
+
+def test_martin_data(db):
     martin_data = db.get_data('Martin')
     assert martin_data['id'] == 1
     assert martin_data['name'] == 'Martin'
     assert martin_data['age'] == 18
 
-def test_joaquin_data():
-    db = StudentDB()
-    db.connect('data.json')
+
+def test_joaquin_data(db):
     martin_data = db.get_data('Joaquin')
     assert martin_data['id'] == 2
     assert martin_data['name'] == 'Joaquin'
     assert martin_data['age'] == 40
+
